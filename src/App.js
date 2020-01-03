2db830ed10f1a6a57fd17e3acb38330b509c3281
Pavan Kumar Patruni <pavanpathro@gmail.com>
2020-01-03 23:35:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isUsernameValid, onUsernameChange] = useValidState("username");
+  const [isPasswordValid, onPasswordChange] = useValidState("password");
+  const [isEmailValid, onEmailChange] = useValidState("email");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label htmlFor="username">Username: </label>
+        <input
+          id="username"
+          onChange={e => onUsernameChange(e)}
+        />
+        {isUsernameValid ? <p>Your Username is valid </p> : null}
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input
+          id="password"
+          onChange={e => onPasswordChange(e)}
+        />
+        {isPasswordValid ? <p>Your Password is valid </p> : null}
+      </div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input
+          id="email"
+          onChange={e => onEmailChange(e)}
+        />
+        {isEmailValid ? <p>Your Email is valid </p> : null}
+      </div>
     </div>
   );
+}
+
+const useValidState = (type) => {
+  const [isValid, setValid] = useState(false);
+
+  const onChange = e => {
+    const newValue = e.target.value;
+    let _isValid = false;
+    switch(type) {
+      case 'username':
+        if (newValue.length >= 3 && newValue.length <= 20) _isValid = true;
+        break;
+      case 'password':
+        if (newValue.length >= 8) _isValid = true;
+        break;
+      case 'email':
+        let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if (reg.test(newValue)) _isValid = true;
+        break;
+      default:
+        if (newValue.length > 1) _isValid = true;
+        break;
+    }
+    
+    setValid(_isValid);
+  };
+
+  return [isValid, onChange];
 }
 
 export default App;
